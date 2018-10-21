8c0ca26834995eeb07bc93913d9391cdcf004450
pierrick.pamart <pierrick.pamart@telecom-sudparis.eu>
2018-10-21 19:20:12 +0200

    Add all the tutorial animations + the dummy

---
+++ Assets/AnimatorController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorController : MonoBehaviour {
+
+
+    [SerializeField] Animator dummyAnimator;
+
+    // Use this for initialization
+    void Start () {
+        dummyAnimator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown("a"))
+            dummyAnimator.SetTrigger("Show_right_wave");
+        if (Input.GetKeyDown("z"))
+            dummyAnimator.SetTrigger("Show_left_wave");
+        if (Input.GetKeyDown("e"))
+            dummyAnimator.SetTrigger("Show_right_punch");
+        if (Input.GetKeyDown("r"))
+            dummyAnimator.SetTrigger("Show_running");
+        if (Input.GetKeyDown("t"))
+            dummyAnimator.SetTrigger("Show_hands_up");
+    }
+}
